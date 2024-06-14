b3a4b56cef431b3043cf1fd99d4bdfa48f428b31
Nadia Rajpoot <nadiarajpoot44@gmail.com>
2024-06-14 11:35:35 -0700

    First week task of fellowship

---
+++ bytewise-project/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div
+      className="App"
+      style={{
+        alignItems: "center",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <h2>Bytewise-Project</h2>
+    </div>
+  );
+}
+
+export default App;
